9cc03525293f4dbb1475e8e130bb4062753e35f4
Kamil.Zydlo <Kamil.Zydlo@PK1-09>
2022-03-11 13:04:55 +0100

    Wersja 1.0

---
+++ lab2/lab2/lab2/Classroom.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    class Classroom
+    {
+
+        private string name;
+        private Classroom[] classrooms;
+        private string v;
+        private Person[] persons;
+
+        public string Name { get => this.name; }
+
+        public Classroom(string name, Classroom[] classrooms )
+        {
+
+            this.name = name;
+            this.classrooms = classrooms;
+
+        }
+
+        public Classroom(string v, Person[] persons)
+        {
+            this.v = v;
+            this.persons = persons;
+        }
+
+        public override string ToString()
+        {
+            return $"Classroom: {base.ToString()}";
+        }
+
+    }
+}
