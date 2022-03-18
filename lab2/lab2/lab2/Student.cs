de9a1ac943ef2104a874e45afa8454ea04a04c4b
Wtykson123 <101329717+Wtykson123@users.noreply.github.com>
2022-03-18 13:14:09 +0100

    Add files via upload

---
+++ lab2/lab2/lab2/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Student : Person
+    {
+
+        protected String group;
+        protected List<Task> tasks;
+
+        public string Group { get => this.group; }
+
+        public Student(String name, int age, String group) : this(name, age, group, new List<Task> ())
+        {
+            this.group = group;
+
+
+        }
+
+        internal void AddTask(string v, TaskStatus waiting)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Student(String name, int age, String group, List<Task> tasks) : base(name, age)
+        {
+            this.group = group;
+            this.tasks = tasks;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Student: {base.ToString()}";
+        }
+
+        internal void UpdateTask(int v, TaskStatus done)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ lab2/lab2/lab2/Task.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public enum TaskStatus
+    {
+
+        Waiting,
+        Progress,
+        Done,
+        Rejeted,
+        Rejected,
+    }
+    public class Task
+    {
+
+        private string name;
+        private TaskStatus status;
+
+        public string Name { get => this.name; }
+        public TaskStatus Status { get => this.status; }
+
+        public Task(string name, TaskStatus status)
+        {
+
+            this.name = name;
+            this.status = status;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Task: {base.ToString()}";
+
+        }
+
+
+
+
+    }
+
+}
