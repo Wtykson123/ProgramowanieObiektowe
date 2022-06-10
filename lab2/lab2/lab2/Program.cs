3b940ce4300449d7a77ed0b0cbc543e41bd71ffe
Wtykson123 <101329717+Wtykson123@users.noreply.github.com>
2022-06-10 10:04:50 +0200

    Update Program.cs

--- lab2/lab2/lab2/Program.cs
+++ lab2/lab2/lab2/Program.cs
@@ -1,173 +1,31 @@
-<<<<<<< HEAD
-﻿using System;
+using lab2;
+using TaskStatus = lab2.TaskStatus;
 
-namespace lab2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Teacher treacher = new Teacher("Maria Skłodowska", 50);
+Teacher treacher = new Teacher("Maria Skłodowska", 50);
 
-            Student student1 = new Student("Jan Kowaslski", 21, "LAB-01");
-            Student student2 = new Student("Jan Kowaslski", 21, "LAB-01");
-            Student student3 = new Student("Jaś Fasola", 23, "LAB-02");
-                                       __
-                                      / _)
-                             _.----._/ /
-                            /         /
-                         __/ (  | (  |
-                        /__.-'|_|--|_|
+Student student1 = new Student("Jan Kowaslski", 21, "LAB-01");
+Student student2 = new Student("Jan Kowaslski", 21, "LAB-01");
+Student student3 = new Student("Jaś Fasola", 23, "LAB-02");
 
-            Person[] persons = { treacher, student1, student2, student3 };
-            Classroom classroom = new Classroom("Sala Komputerowa", persons);
+student1.AddTask("Taks A", TaskStatus.Waiting);
+student1.AddTask("Taks B", TaskStatus.Waiting);
+student1.AddTask("Taks C", TaskStatus.Rejected);
 
-            Console.WriteLine(classroom);
+student2.AddTask("Taks A", TaskStatus.Waiting);
+student2.AddTask("Taks B", TaskStatus.Waiting);
+student2.AddTask("Taks C", TaskStatus.Rejected);
 
-            Console.WriteLine("student1 == student2: " + student1.Equals(student2)); // Output: student1 == student2: true
-            Console.WriteLine("student1 == student3: " + student1.Equals(student3)); // Output: student1 == student3: false
-        
-    }
-    }
-    public class Person: IEquatable<Person>
-    {
+student3.AddTask("Taks D", TaskStatus.Done);
+student3.AddTask("Taks E", TaskStatus.Waiting);
+student3.AddTask("Taks F", TaskStatus.Waiting);
 
-        protected String name;
-        protected int age;
+student3.UpdateTask(1, TaskStatus.Done);
+student3.UpdateTask(2, TaskStatus.Progress);
 
-        public String Name { get => this.name; }
-        public int Age { get => this.age; }
+Person[] persons = { treacher, student1, student2, student3 };
+Classroom classroom = new Classroom("Sala Komputerowa", persons);
 
-        public Person(String name, int age)
-        {
-            this.name = name;
-            this.age = age;
+Console.WriteLine(classroom);
 
-        }
-        public bool Equals(Person other)
-        {
-
-            if (other == null) return false;
-            if (other == this) return true;
-
-            return Object.Equals(this.name, other.name) && Object.Equals(other.name, other.age);
-
-        }
-
-        public override string ToString()
-        {
-            return $"(this.name) ({this.age} y.o.)";
-        }
-
-    }
-
-    public class Teacher : Person
-    {
-    
-
-        public Teacher(String name, int age):base(name,age)
-        {
-               
-        
-        }
-        
-        public override string ToString()
-        {
-            return $"Teacher: {base.ToString()}";
-          
-        }
-    }
-
-
-
-
-
-
-}
-=======
-﻿using System;
-
-namespace lab2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Teacher treacher = new Teacher("Maria Skłodowska", 50);
-
-            Student student1 = new Student("Jan Kowaslski", 21, "LAB-01");
-            Student student2 = new Student("Jan Kowaslski", 21, "LAB-01");
-            Student student3 = new Student("Jaś Fasola", 23, "LAB-02");
-                                       __
-                                      / _)
-                             _.----._/ /
-                            /         /
-                         __/ (  | (  |
-                        /__.-'|_|--|_|
-
-            Person[] persons = { treacher, student1, student2, student3 };
-            Classroom classroom = new Classroom("Sala Komputerowa", persons);
-
-            Console.WriteLine(classroom);
-
-            Console.WriteLine("student1 == student2: " + student1.Equals(student2)); // Output: student1 == student2: true
-            Console.WriteLine("student1 == student3: " + student1.Equals(student3)); // Output: student1 == student3: false
-        
-    }
-    }
-    public class Person: IEquatable<Person>
-    {
-
-        protected String name;
-        protected int age;
-
-        public String Name { get => this.name; }
-        public int Age { get => this.age; }
-
-        public Person(String name, int age)
-        {
-            this.name = name;
-            this.age = age;
-
-        }
-        public bool Equals(Person other)
-        {
-
-            if (other == null) return false;
-            if (other == this) return true;
-
-            return Object.Equals(this.name, other.name) && Object.Equals(other.name, other.age);
-
-        }
-
-        public override string ToString()
-        {
-            return $"(this.name) ({this.age} y.o.)";
-        }
-
-    }
-
-    public class Teacher : Person
-    {
-    
-
-        public Teacher(String name, int age):base(name,age)
-        {
-               
-        
-        }
-        
-        public override string ToString()
-        {
-            return $"Teacher: {base.ToString()}";
-          
-        }
-    }
-
-
-
-
-
-
-}
->>>>>>> 20a94b11db97ee0397c0cd48574142054d95e370
+Console.WriteLine("student1 == student2: " + student1.Equals(student2)); // Output: student1 == student2: true
+Console.WriteLine("student1 == student3: " + student1.Equals(student3)); // Output: student1 == student3: false
